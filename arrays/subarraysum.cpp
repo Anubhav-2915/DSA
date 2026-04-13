90215fad61dc365fc5e48b6793d6dd4c721d5003
Anubhav <anubhavg2915@gmail.com>
2026-04-13 22:36:43 +0530

    .

---
+++ arrays/subarraysum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+    
+    
+    
+int subarraySum(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> prefixsum(n,0);
+    prefixsum[0] = nums[0];
+    for(int i = 1; i<n; i++){
+        prefixsum[i] = prefixsum[i-1]+nums[i];
+        }
+
+    unordered_map<int,int> m;
+    int count = 0;
+        for(int j = 0; j<n; j++){
+            if(prefixsum[j] == k) count++;
+            int val = prefixsum[j] - k;
+            if(m.find(val)!= m.end()){
+                count+= m[val];
+            }
+            // if(m.find(val)== m.end()){
+            //     m[prefixsum[j]] = 0;
+            // }
+            m[prefixsum[j]]++;
+        }
+        return count;
+    }
